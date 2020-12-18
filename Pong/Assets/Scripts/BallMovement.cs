03010eae34e1461da42df919607bbe14290b33b3
Josue-Gonzalez-Juan <josgonzalez@csumb.edu>
2020-12-17 17:04:43 -0800

    Final Push

---
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float x;
+    public float z;
+    public float speed;
+
+    private Rigidbody ballRigidbody;
+    private Transform ballTransform;
+
+    public GameObject scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = Random.Range(0, 2) == 0 ? -1 : 1;
+        z = Random.Range(0, 2) == 0 ? -1 : 1;
+        speed = 5f;
+        ballTransform = GetComponent<Transform>();
+        ballRigidbody = GetComponent<Rigidbody>();
+        ballRigidbody.velocity = new Vector3(speed * x, 0, speed * z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PaddleLeft" || collision.gameObject.name == "PaddleRight")
+        {
+            speed += 1f;
+            x = Random.Range(0, 2) == 0 ? -1 : 1;
+            z = Random.Range(0, 2) == 0 ? -1 : 1;
+            ballRigidbody.velocity = new Vector3(speed * x, 0, speed * z);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        ballTransform.position = new Vector3(0, 0.5f, 0);
+        speed = 5f;
+        if (collider.gameObject.name == "RightGoal")
+        {
+            //Debug.Log("Left Scored");
+            x = -1f;
+            z = Random.Range(0, 2) == 0 ? -1 : 1;
+            ballRigidbody.velocity = new Vector3(speed * x, 0, speed * z);
+            scoreManager.GetComponent<ScoreManager>().Scored(false);
+        }
+        if (collider.gameObject.name == "LeftGoal")
+        {
+            //Debug.Log("Right Scored");
+            x = 1f;
+            z = Random.Range(0, 2) == 0 ? -1 : 1;
+            ballRigidbody.velocity = new Vector3(speed * x, 0, speed * z);
+            scoreManager.GetComponent<ScoreManager>().Scored(true);
+        }
+    }
+}
---
+++ Pong/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text player1;
+    private int scoreP1;
+    public Text player2;
+    private int scoreP2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreP1 = 0;
+        scoreP2 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scored(bool player)
+    {
+        if (!player)
+        {
+            scoreP1++;
+            player1.text = "" + scoreP1;
+            if (scoreP1 >= 3)
+            {
+                GameWin("Player 1");
+            }
+            else
+            {
+                Debug.Log("Player 1 Scored\n" + "Score is " + scoreP1 + "-" + scoreP2);
+            }
+        }
+        if (player)
+        {
+            scoreP2++;
+            player2.text = "" + scoreP2;
+            if (scoreP2 >= 3)
+            {
+                GameWin("Player 2");
+            }
+            else
+            {
+                Debug.Log("Player 2 Scored\n" + "Score is " + scoreP1 + "-" + scoreP2);
+            }
+        }
+    }
+
+    void GameWin(string winner)
+    {
+        Debug.Log(winner + " Won!");
+        scoreP1 = 0;
+        scoreP2 = 0;
+        player1.text = "00";
+        player2.text = "00";
+    }
+}
